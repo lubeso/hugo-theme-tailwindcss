0185f47fe55d5e28b51b964e9f51b2e3e3bcf7f5
Luis Becerra Solis <lubeso@icloud.com>
2024-07-06 13:20:13 -0400

    Chore: Add TailwindCSS and PostCSS configs

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/**/*.html'
+    './assets/**/*.css',
+  ],
+  theme: {
+    extend: {
+      fontFamily: ['serif'],
+    },
+  },
+  plugins: [],
+}
